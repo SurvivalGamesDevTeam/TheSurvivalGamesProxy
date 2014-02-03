1e6c07ce955b2fd84015e52813d88b45cb99e83b
Dylan Ryman <dylan@dtr-world.com>
2014-02-02 21:28:14 -0500

    Added basic plugin skeleton

---
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/TheSurvivalGamesProxy.java
@@ -0,0 +1,15 @@
+package com.communitysurvivalgames.thesurvivalgames;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class TheSurvivalGamesProxy extends Plugin {
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
